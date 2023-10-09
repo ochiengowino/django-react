94d44f83ea5e3e0563a4be9787b71ebf325327cd
ochiengowino <benjaminochieng99@gmail.com>
2023-10-09 13:31:59 +0300

    backend config

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class HealthFacilities(models.Model):
+    name = models.CharField(max_length=80)
+    healthcare = models.CharField(max_length=80)
+    amenity = models.CharField(max_length=80)
+    operator: type = models.CharField(max_length=80)
+    geom = models.PointField(srid=4326)
+
+
+# Auto-generated `LayerMapping` dictionary for HealthFacilities model
+# healthfacilities_mapping = {
+#     'name': 'name',
+#     'healthcare': 'healthcare',
+#     'amenity': 'amenity',
+#     'operator:type': 'operator:type',
+#     'geom': 'POINT',
+# }
